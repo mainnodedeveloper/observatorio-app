5e779c9d3d0984e6052c9e068b0c606bd9200dec
mainnodedeveloper <mainnodedeveloper@gmail.com>
2024-08-07 14:52:52 -0500

    bootstrap

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { AppService } from './app/app.service';
+import * as bootstrap from 'bootstrap'
 
 bootstrapApplication(AppComponent, {
   providers: [AppService],
--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,15 @@ import { angular } from '@nitedani/vite-plugin-angular/plugin';
 
 export default defineConfig({
   plugins: [angular()],
+  root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  server: {
+    port: 5000,
+    hot: true
+  }
+
 });
