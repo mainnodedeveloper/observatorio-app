e25af02004677cb93c955edf2e977a5911e2cc06
mainnodedeveloper <mainnodedeveloper@gmail.com>
2024-08-07 16:18:39 -0500

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { AppService } from './app.service';
 import { MaterialModule } from './material.module';
 
+import Button from 'bootstrap/js/dist/button';
+import Alert from 'bootstrap/js/dist/alert';
+
+import { Tooltip, Toast, Popover } from 'bootstrap';
+
+
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [MaterialModule],
+  imports: [MaterialModule, Button ],
 })
 export class AppComponent {
   constructor(private appService: AppService) {
